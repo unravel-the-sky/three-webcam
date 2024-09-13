b820336f8ced16f8297bb12ade89d5b16df4f6e0
unravel-the-sky <sekdemir@gmail.com>
2024-09-13 15:59:42 +0200

    add types and stuff

--- three-webcam-app/app/components/client/ShowTime.tsx
+++ three-webcam-app/app/components/client/ShowTime.tsx
@@ -6,7 +6,7 @@ import {
   useFrame,
   ThreeElements,
   useLoader,
-  Vector3,
+  Vector3 as FiberVector3,
 } from "@react-three/fiber";
 import { useEffect, useRef, useState } from "react";
 import {
@@ -22,9 +22,15 @@ import {
   useBox,
   usePlane,
   useSphere,
+  Triplet,
 } from "@react-three/cannon";
 
-export default function ShowTime({ imgList }: { imgList: string[] }) {
+// Type for Props
+interface ShowTimeProps {
+  imgList: string[];
+}
+
+export default function ShowTime({ imgList }: ShowTimeProps) {
   return (
     <div className="flex-1 bg-slate-200">
       <Canvas
@@ -38,8 +44,15 @@ export default function ShowTime({ imgList }: { imgList: string[] }) {
   );
 }
 
-const PhyPlane = ({ color, ...props }) => {
-  const [ref] = usePlane(() => ({ ...props }));
+// PhyPlane component
+interface PhyPlaneProps {
+  color: string;
+  position?: Triplet;
+  rotation?: Triplet;
+}
+
+const PhyPlane = ({ color, ...props }: PhyPlaneProps) => {
+  const [ref] = usePlane<THREE.Mesh>(() => ({ ...props }));
 
   return (
     <Plane args={[1000, 1000]} ref={ref}>
@@ -48,9 +61,15 @@ const PhyPlane = ({ color, ...props }) => {
   );
 };
 
-const PhySphere = (props) => {
+// PhySphere component
+interface PhySphereProps {
+  imgUrl: string;
+  position?: Triplet;
+}
+
+const PhySphere = (props: PhySphereProps) => {
   const sphereSize = 1;
-  const [ref, api] = useSphere(() => ({
+  const [ref, api] = useSphere<THREE.Mesh>(() => ({
     args: [sphereSize],
     mass: 2,
     ...props,
@@ -78,8 +97,18 @@ const PhySphere = (props) => {
   );
 };
 
-const PhyBox = (props) => {
-  const [ref, api] = useBox(() => ({ args: [2, 2, 2], mass: 1, ...props }));
+// PhyBox component
+interface PhyBoxProps {
+  imgUrl: string;
+  position?: Triplet;
+}
+
+const PhyBox = (props: PhyBoxProps) => {
+  const [ref, api] = useBox<THREE.Mesh>(() => ({
+    args: [2, 2, 2],
+    mass: 1,
+    ...props,
+  }));
   const colorMap = useTexture(props.imgUrl) as THREE.Texture;
 
   return (
@@ -102,18 +131,17 @@ const PhyBox = (props) => {
   );
 };
 
-const Scene = ({ imgList }: { imgList: string[] }) => {
+// Scene component
+const Scene = ({ imgList }: ShowTimeProps) => {
   return (
     <>
       <Physics gravity={[0, -10, 0]} allowSleep>
-        {/* <Debug scale={1} color="black"> */}
         <PhyPlane
           color="yellow"
           position={[0, -2, 4]}
           rotation={[-Math.PI / 2, 0, 0]}
         />
-        {/* </Debug> */}
-        {imgList.map((imgUrl, index) => (
+        {imgList.map((imgUrl) => (
           <PhyBox
             imgUrl={imgUrl}
             key={imgUrl}
@@ -127,51 +155,33 @@ const Scene = ({ imgList }: { imgList: string[] }) => {
       </Physics>
       <ambientLight intensity={0.6} />
       <directionalLight />
-      {/* {imgList.map((imgUrl, index) => (
-        <PlayerBall
-          imgUrl={imgUrl}
-          key={imgUrl}
-          position={[(Math.random() - 0.5) * 20, (Math.random() - 0.5) * 4, 0]}
-        />
-      ))} */}
     </>
   );
 };
 
-const PlayerBall = ({
-  imgUrl,
-  position,
-}: {
+// PlayerBall component
+interface PlayerBallProps {
   imgUrl: string;
-  position: Vector3 | undefined;
-}) => {
+  position: FiberVector3 | undefined;
+}
+
+const PlayerBall = ({ imgUrl, position }: PlayerBallProps) => {
   const colorMap = useLoader(THREE.TextureLoader, imgUrl);
-
   const meshRef = useRef<THREE.Mesh>(null!);
-
-  // useFrame((state, delta) => {
-  //   meshRef.current.rotation.x += delta;
-  //   meshRef.current.rotation.y += delta;
-  // });
 
   useEffect(() => {
     if (meshRef.current) meshRef.current.rotation.y = Math.PI * 1.5;
   }, []);
 
   return (
-    <mesh
-      ref={meshRef}
-      position={position}
-      onClick={() => {
-        api.applyImpulse([0, 10, 0], [0, 0, 0]);
-      }}
-    >
+    <mesh ref={meshRef} position={position}>
       <sphereGeometry args={[1, 32, 32]} />
       <meshStandardMaterial map={colorMap} />
     </mesh>
   );
 };
 
+// TestScene component
 const TestScene = () => {
   return (
     <Canvas className="h-2xl w-2xl">
@@ -190,6 +200,7 @@ const TestScene = () => {
   );
 };
 
+// CustomBox component
 const CustomBox = (props: ThreeElements["mesh"]) => {
   const meshRef = useRef<THREE.Mesh>(null!);
   const [hovered, setHover] = useState(false);
@@ -200,9 +211,9 @@ const CustomBox = (props: ThreeElements["mesh"]) => {
       {...props}
       ref={meshRef}
       scale={active ? 1.5 : 1}
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}
+      onClick={() => setActive(!active)}
+      onPointerOver={() => setHover(true)}
+      onPointerOut={() => setHover(false)}
     >
       <boxGeometry args={[1, 1, 1]} />
       <meshStandardMaterial color={hovered ? "hotpink" : "#2f74c0"} />
