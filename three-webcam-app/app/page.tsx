c45b48defaa6932c80b10b60ce270c5b10c601ec
unravel-the-sky <sekdemir@gmail.com>
2024-09-23 18:08:46 +0200

    offline the page for public use

--- three-webcam-app/app/page.tsx
+++ three-webcam-app/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import AppWrapper from "./components/client/AppWrapper";
 import ChannelWrapper from "./components/client/ChanellWrapper";
 
@@ -5,7 +6,8 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-4 justify-center items-center">
       <ChannelWrapper>
-        <AppWrapper />
+        <Button variant={"ghost"}>offline :shrug:</Button>
+        {/* <AppWrapper /> */}
       </ChannelWrapper>
     </div>
   );
