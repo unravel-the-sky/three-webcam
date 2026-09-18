be9d89d6c4b30b327cfe8bb8a9001273ce598218
unravel-the-sky <sekdemir@gmail.com>
2026-09-18 19:09:24 +0100

    reactivate

--- three-webcam-app/app/page.tsx
+++ three-webcam-app/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-4 justify-center items-center">
       <ChannelWrapper>
-        <Button variant={"ghost"}>offline :shrug:</Button>
-        {/* <AppWrapper /> */}
+        {/* <Button variant={"ghost"}>offline :shrug:</Button> */}
+        <AppWrapper />
       </ChannelWrapper>
     </div>
   );
